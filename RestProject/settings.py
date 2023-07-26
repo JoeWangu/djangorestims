333429ab47d13a104708cbda6b0238b54211efa9
wangu <joewangu2021@outlook.com>
2023-07-26 21:17:11 +0300

    update allowed hosts in settings

--- RestProject/settings.py
+++ RestProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5gky#43sk$4u+@e8%@wv7aep=*k(*d%ok3)=mu4mi_szz$bd_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['royaldweeb.pythonanywhere.com']
 
 
 # Application definition
